a97f4685b51850b54fa969966810992c291e9877
Kerrymas <patienceyonah@gmail.com>
2023-06-19 11:40:37 -0700

    q3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printts the name of  the file it was compiled from
+ *
+ * Return: nothing to be returned
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
