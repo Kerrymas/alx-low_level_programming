ed0a9a8eebf21e05e557cb7564cd37a3bddc1c91
Kerrymas <patienceyonah@gmail.com>
2023-06-30 17:31:58 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_intro - function that prints the introduction lines
+ */
+
+void print_intro(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
