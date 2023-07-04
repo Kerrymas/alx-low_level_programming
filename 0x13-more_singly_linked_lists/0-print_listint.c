4643e3408bb957ef89f3c84c7b4fc8f9a4581429
Kerrymas <patienceyonah@gmail.com>
2023-07-04 00:55:10 -0700

    q0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *current = h;
 	size_t node_count = 0;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
